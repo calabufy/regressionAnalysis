aad1db72ad997e5eaaf0bfaf229c32ad54054e3a
Anton Shchukin <Antonovka2310@yandex.ru>
2024-06-25 11:26:37 +0300

    Переобучение ARIMA-модели
     При анализе предыдущей версии модели ARIMA было
     обнаружено переобучение модели. Это может
     привести к некорректному предсказанию будущих
     результатов.
    
     Было проведено разбиение выборки на 2 части:
     на обучающую и тестовую. ARIMA-модель обучалась
     по первым элементам выборки и строила
     предсказание для последних 30 значений выборки,
     затем производилась метрика для тестовой части.
     Новые параметры ARIMA(8, 1, 10) показывают
     меньшую среднеквадратичную ошибку, чем полином
     (0.1046 против 0.1643), поэтому можем сказать,
     что ARIMA-модель удачна.

--- main.py
+++ main.py
@@ -54,33 +54,51 @@ beta = np.linalg.inv(X.T @ X) @ (X.T @ chlor_a)  # X.T @ X @ beta = X.T @ y
 trend = beta[0] * time_num + beta[1]  # y = k * x + b
 
 # Построение модели ARIMA
+# Разделение выборки на 2 части: обучающая и тестовая
+train_chlor_a, test_chlor_a = chlor_a[:-30], chlor_a[-30:]
+train_chlor_a_pandas = pd.Series(train_chlor_a)
 # Определение параметра d (параметр дифференциации), используем ADF-тест
 # Проверка на стационарность
-result = adfuller(chlor_a_pandas)
+result = adfuller(train_chlor_a_pandas)
 # print(result)
 # result : (тестовая статстика ADF, p-value (если значение меньше уровня значимости, то ряд стационарен),
 #           количество лагов, количество фактических наблюдений, использованных для теста после учета лагов,
 #           критические значения для различных уровней значимости (1%, 5%, 10%), ...)
-if result[1] > 0.05:  # Если временной ряд не стационарен
-    chlor_a_pandas_diff = chlor_a_pandas.diff(periods=1).dropna()
-    # Временной ряд необходимо привести к стационарному, тк в при нестационарном ряде его среднее значение
-    # со временем может измениться, что делает моделирование менее надёжным
-    result = adfuller(chlor_a_pandas_diff)
-else:  # Если временной ряд стационарен
-    chlor_a_pandas_diff = chlor_a_pandas_av
+train_chlor_a_pandas_diff = train_chlor_a_pandas.diff(periods=1).dropna()
+# Временной ряд необходимо привести к стационарному, тк в при нестационарном ряде его среднее значение
+# со временем может измениться, что делает моделирование менее надёжным (проверка на стационарность: p-value <= 0.05)
+result = adfuller(train_chlor_a_pandas_diff)
 # print(result)
 
 # порядки AR и MA (нахождение p и q)
-plot_acf(chlor_a_pandas_diff)
-plot_pacf(chlor_a_pandas_diff)
+plot_acf(train_chlor_a_pandas_diff)
+plot_pacf(train_chlor_a_pandas_diff)
 
-d = 1   # порядок интегрирования (I). Он равен 1, тк ряд стационарен
-p = 17  # порядок авторегрессии (AR)
-q = 40  # порядок скользящего среднего (MA)
+'''d = 1   # порядок интегрирования (I). Он равен 1, тк ряд приведён к стационарному через одну дифференциацию
 
-model_ARIMA = ARIMA(chlor_a, order=(p, d, q)).fit()
-print(model_ARIMA.summary())
+best_pdq = None
+min_rmse = float('inf')
+
+for p in range(1, 11):
+    for q in range(1, 11):
+        print(f"ARIMA({p}, {d}, {q})")
+        try:
+            model_ARIMA = ARIMA(train_chlor_a, order=(p, d, q)).fit()
+            pred = model_ARIMA.predict(start=len(train_chlor_a), end=len(train_chlor_a) + len(test_chlor_a) - 1)
+            rmse_ARIMA = np.sqrt(mean_squared_error(test_chlor_a, pred))
+            if rmse_ARIMA < min_rmse:
+                best_pdq = (p, d, q)
+                min_rmse = rmse_ARIMA
+        except:
+            continue
+
+print(f"Best pdq: {best_pdq}, Min RMSE: {min_rmse}")'''
+
+p, d, q = 8, 1, 10  # best_pdq
+
+model_ARIMA = ARIMA(train_chlor_a, order=(p, d, q)).fit()
 residuals = model_ARIMA.resid[1:]
+        
 fig, ax = plt.subplots(1, 2)
 
 # Построение графика остаточных значений и их плотности (проверка на белый шум)
@@ -92,12 +110,14 @@ plt.figure(figsize=(10, 5))
 plt.plot(time, chlor_a, marker=',', linestyle='-', color='gray', alpha=0.5, label='chlorophyll A')
 plt.plot(time, moving_av, marker=',', linestyle='-', color='green', label='moving av (without season cycle)')
 plt.plot(time, trend, color='red', linewidth=2.5, label='Trend Line')
-pred = model_ARIMA.predict(start=1, end=len(chlor_a)-1)
-rmse_ARIMA = np.sqrt(mean_squared_error(chlor_a[1:], pred))
+train_ARIMA = model_ARIMA.predict(start=1, end=len(train_chlor_a)-1)
+pred_ARIMA = model_ARIMA.predict(start=len(train_chlor_a), end=len(train_chlor_a) + len(test_chlor_a) - 1)
+rmse_ARIMA = np.sqrt(mean_squared_error(test_chlor_a, pred_ARIMA))
 rmse_trend = np.sqrt(mean_squared_error(chlor_a, trend))
 print(f"RMSE for ARIMA({p}, {d}, {q}) : {rmse_ARIMA}")
 print(f"RMSE for Trend line: {rmse_trend}")
-plt.plot(time[1:], pred, color='blue', linestyle='--', linewidth=1, label='ARIMA Prediction')
+plt.plot(time[1:len(train_chlor_a)], train_ARIMA, color='blue', linestyle='-', linewidth=1, label='ARIMA train')
+plt.plot(time[len(train_chlor_a):], pred_ARIMA, color='blue', linestyle='--', linewidth=1, label='ARIMA test')
 
 # Настройка графика
 plt.title('Chlorophyll-a Concentration Over Time')
