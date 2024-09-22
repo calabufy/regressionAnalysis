71da5518e5d7e7b72198dd8a46b66edc1f7130d1
Anton Shchukin <Antonovka2310@yandex.ru>
2024-09-22 13:28:32 +0300

    Предикт ARIMA-модели
     В данной версии кода было реализованно
     построение прогноза концентрации Хлорофилла А
     на ближайшие 5 лет с шагом в месяц

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
 import warnings
-warnings.filterwarnings("ignore")
 
+warnings.filterwarnings("ignore")
 
 filename = "omi_health_chl_baltic_oceancolour_area_averaged_mean_19970901_P20230807.nc"
 dataset = Dataset(filename, 'r')
@@ -61,54 +61,22 @@ trend = beta[0] * time_num + beta[1]  # y = k * x + b
 # Разделение выборки на 2 части: обучающая и тестовая
 train_chlor_a, test_chlor_a = chlor_a[:-30], chlor_a[-30:]
 train_chlor_a_pandas = pd.Series(train_chlor_a)
+
 # Определение параметра d (параметр дифференциации), используем ADF-тест
-# Проверка на стационарность
 result = adfuller(train_chlor_a_pandas)
-# print(result)
-# result : (тестовая статстика ADF, p-value (если значение меньше уровня значимости, то ряд стационарен),
-#           количество лагов, количество фактических наблюдений, использованных для теста после учета лагов,
-#           критические значения для различных уровней значимости (1%, 5%, 10%), ...)
 train_chlor_a_pandas_diff = train_chlor_a_pandas.diff(periods=1).dropna()
-# Временной ряд необходимо привести к стационарному, тк в при нестационарном ряде его среднее значение
-# со временем может измениться, что делает моделирование менее надёжным (проверка на стационарность: p-value <= 0.05)
 result = adfuller(train_chlor_a_pandas_diff)
-# print(result)
 
 # порядки AR и MA (нахождение p и q)
 plot_acf(train_chlor_a_pandas_diff)
 plot_pacf(train_chlor_a_pandas_diff)
 
-'''d = 1   # порядок интегрирования (I). Он равен 1, тк ряд приведён к стационарному через одну дифференциацию
-
-best_pdq_rmse = None
-best_pdq_AIC = None
-min_rmse = float('inf')
-min_AIC = float('inf')
-
-for p in range(1, 11):
-    for q in range(1, 11):
-        print(f"ARIMA({p}, {d}, {q})")
-        try:
-            model_ARIMA = ARIMA(train_chlor_a, order=(p, d, q)).fit()
-            pred = model_ARIMA.predict(start=len(train_chlor_a), end=len(train_chlor_a) + len(test_chlor_a) - 1)
-            rmse_ARIMA = np.sqrt(mean_squared_error(test_chlor_a, pred))
-            if rmse_ARIMA < min_rmse:
-                best_pdq_rmse = (p, d, q)
-                min_rmse = rmse_ARIMA
-            if model_ARIMA.aic < min_AIC:
-                best_pdq_AIC = (p, d, q)
-                min_AIC = model_ARIMA.aic
-        except:
-            continue
-
-print(f"RMSE: Best pdq: {best_pdq_rmse}, Min RMSE: {min_rmse}")
-print(f"AIC: Best pdq: {best_pdq_AIC}, Min AIC: {min_AIC}")'''
-
-p, d, q = 8, 1, 10  # best_pdq from RMSE and AIC
+# Параметры модели ARIMA (на основе AIC и RMSE)
+p, d, q = 8, 1, 10
 
 model_ARIMA = ARIMA(train_chlor_a, order=(p, d, q)).fit()
 residuals = model_ARIMA.resid[1:]
-        
+
 fig, ax = plt.subplots(1, 3, figsize=(10, 5))
 
 # Построение графика остаточных значений, их плотности и qqplot
@@ -120,22 +88,26 @@ ax[0].set_title("Residuals")
 ax[1].set_title("Density")
 ax[2].set_title("QQplot")
 
+# Количество месяцев для прогноза на 5 лет (60 месяцев)
+forecast_months = 5 * 12
+
+# Создание временных меток для прогноза на 5 лет
+future_time = pd.date_range(start=time[-1], periods=forecast_months + 1, freq='M')[1:]
+
 # Построение графика
 plt.figure(figsize=(10, 5))
 plt.plot(time, chlor_a, marker=',', linestyle='-', color='gray', alpha=0.5, label='chlorophyll A')
-plt.plot(time, moving_av, marker=',', linestyle='-', color='green', label='moving av (without season cycle)')
+# plt.plot(time, moving_av, marker=',', linestyle='-', color='green', label='moving av (without season cycle)')
 plt.plot(time, trend, color='red', linewidth=2.5, label='Trend Line')
-train_ARIMA = model_ARIMA.predict(start=1, end=len(train_chlor_a)-1)
+train_ARIMA = model_ARIMA.predict(start=1, end=len(train_chlor_a) - 1)
 pred_ARIMA = model_ARIMA.predict(start=len(train_chlor_a), end=len(train_chlor_a) + len(test_chlor_a) - 1)
-rmse_ARIMA = np.sqrt(mean_squared_error(test_chlor_a, pred_ARIMA))
-rmse_trend = np.sqrt(mean_squared_error(chlor_a, trend))
-print(f"RMSE for ARIMA({p}, {d}, {q}) : {rmse_ARIMA}")
-print(f"RMSE for Trend line: {rmse_trend}")
+forecast_ARIMA = model_ARIMA.predict(start=len(chlor_a), end=len(chlor_a) + forecast_months - 1)
 plt.plot(time[1:len(train_chlor_a)], train_ARIMA, color='blue', linestyle='-', linewidth=1, label='ARIMA train')
 plt.plot(time[len(train_chlor_a):], pred_ARIMA, color='blue', linestyle='--', linewidth=1, label='ARIMA test')
+plt.plot(future_time, forecast_ARIMA, color='orange', linestyle='--', linewidth=1, label='ARIMA forecast')
 
 # Настройка графика
-plt.title('Chlorophyll-a Concentration Over Time')
+plt.title('Chlorophyll-a Concentration Over Time and ARIMA-model')
 plt.ylabel('Chlorophyll-a Concentration')
 plt.grid(True)
 plt.legend()
